49055b11906d2a7349e26a4bd1df26ea42ff7e96
Wasch <w.ndaiga@gmail.com>
2017-01-13 12:04:54 +0300

    user sandbox flow

--- app/user/views.py
+++ app/user/views.py
@@ -1,25 +1,30 @@
 from flask import render_template, flash
 from .forms import AddPersonForm
+from ..auth.forms import EmailRememberMeForm
 from ..models import Person
 from sqlalchemy import and_
-from flask_login import login_required
+from flask_login import login_required, current_user
 from . import user
 
 
 @user.route('/dashboard/<user>', methods=['GET', 'POST'])
 @login_required
 def dashboard(user):
-    form = AddPersonForm()
-    if form.validate_on_submit():
-        person_exists = Person.query.filter(and_(
-            baptism_name=form.baptism_name.data,
-            ethnic_name=form.ethnic_name.data,
-            surname=form.surname.data,
-            sex=form.sex.data,
-            dob=form.date_of_birth.data,
-            email=form.email.data)).first()
-        if person_exists:
-            flash('Success!')
-        else:
-            flash('Person Already Exists')
-    return render_template('user/dashboard.html', user=user, form=form)
+    if current_user.baptism_name == user or \
+       current_user.email.split('@')[0] == user:
+        form = AddPersonForm()
+        if form.validate_on_submit():
+            person_exists = Person.query.filter(and_(
+                baptism_name=form.baptism_name.data,
+                ethnic_name=form.ethnic_name.data,
+                surname=form.surname.data,
+                sex=form.sex.data,
+                dob=form.date_of_birth.data,
+                email=form.email.data)).first()
+            if person_exists:
+                flash('Success!')
+            else:
+                flash('Person Already Exists')
+        return render_template('user/dashboard.html', user=user, form=form)
+    flash('Please sign in to access profile.')
+    return render_template('auth/welcome.html', form=EmailRememberMeForm())
