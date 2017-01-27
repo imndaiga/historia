1cea481bf2f61f42624a217951d9b4ed1de49469
squarenomad <w.ndaga@gmail.com>
2017-01-27 16:36:37 +0300

    improved component encapsulation

--- static/js/script.js
+++ static/js/script.js
@@ -4,37 +4,115 @@ Vue.component('app-panel', {
 		header: {
 			type: String,
 			required: true
-		},
-		menus: {
-			type: Array
-		},
-		panel_list: {
-			type: Array,
-			required: true
+		}
+	},
+	data: function() {
+		return {
+			menus: [
+				{
+					panel: 'Relationships',
+					reference: 'List all your relationships',
+					icon: 'fa fa-users fa-lg',
+					class: 'btn btn-lg btn-primary btn-block',
+					length: 'col-md-4 col-sm-4 col-xs-4'
+				},
+				{
+					panel: 'Relationships',
+					reference: 'Add a relationship',
+					icon: 'fa fa-user-plus fa-lg',
+					class: 'btn btn-lg btn-info btn-block',
+					length: 'col-md-4 col-sm-4 col-xs-4'
+				},
+				{
+					panel: 'Relationships',
+					reference: 'Edit a relationship',
+					icon: 'fa fa-pencil-square fa-lg',
+					class: 'btn btn-lg btn-warning btn-block',
+					length: 'col-md-4 col-sm-4 col-xs-4'
+				}
+			]
+		}
+	},
+	computed: {
+		receivedPanels: function() {
+			for (i = 0; i < this.menus.length; i++) {
+				var items = [];
+				items.push(this.menus[i].panel)
+				return items
+			}
 		}
 	}
 });
 
 Vue.component('app-navbar', {
 	template: '#app-navbar',
-	props: {
-		title: {
-			type: String,
-			required: true
-		},
-		menus: {
-			type: Array,
-			required: true
+	data: function() {
+		return {
+			title: 'MIMINANI',
+			menus: [
+				{
+					caption: 'Info',
+					icon: 'fa fa-info-circle fa-lg',
+					link: '#',
+					class: '',
+					dropdown: [],
+					reference: 'Read more about the miminani project'
+				},
+				{
+					caption: 'Github',
+					icon: 'fa fa-github fa-lg',
+					link: 'http://github.com/squarenomad/miminani',
+					class: '',
+					dropdown: [],
+					reference: 'View our open source code'
+				},
+				{
+					caption: 'Messages',
+					icon: 'fa fa-inbox fa-lg',
+					link: '#',
+					class: '',
+					dropdown: [],
+					reference: 'Notifications and alerts'
+				},
+				{
+					caption: 'User',
+					icon: 'fa fa-user-circle fa-lg',
+					link: '#',
+					class: 'dropdown active',
+					dropdown: [
+						{
+							caption:'Profile',
+							link: '#',
+							class: 'disabled'
+						},
+						{
+							caption:'Settings',
+							link: '#',
+							class: 'disabled'
+						},
+						{
+							caption:'Sign Out',
+							link: '#',
+							class: ''
+						}
+					],
+					reference: 'Manage your profile'
+				}
+		],
 		}
 	}
 });
 
 Vue.component('app-sidebar', {
 	template: "#app-sidebar",
-	props: {
-		panels: {
-			type: Array,
-			required: true
+	data: function() {
+		return {
+			panels: [
+				'Overview',
+				'Relationships',
+				'Visualisation',
+				'Share'
+		],
 		}
 	}
 });
@@ -42,94 +120,6 @@ Vue.component('app-sidebar', {
 var vm = new Vue({
 	el: '#app',
 	data: {
-		panel_list: [
-			'Overview',
-			'Relationships',
-			'Visualisation',
-			'Share'
-		],
-		menu_items: [
-			{
-				caption: 'Info',
-				icon: 'fa fa-info-circle fa-lg',
-				link: '#',
-				class: '',
-				dropdown: [],
-				reference: 'Read more about the miminani project'
-			},
-			{
-				caption: 'Github',
-				icon: 'fa fa-github fa-lg',
-				link: 'http://github.com/squarenomad/miminani',
-				class: '',
-				dropdown: [],
-				reference: 'View our open source code'
-			},
-			{
-				caption: 'Messages',
-				icon: 'fa fa-inbox fa-lg',
-				link: '#',
-				class: '',
-				dropdown: [],
-				reference: 'Notifications and alerts'
-			},
-			{
-				caption: 'User',
-				icon: 'fa fa-user-circle fa-lg',
-				link: '#',
-				class: 'dropdown active',
-				dropdown: [
-					{
-						caption:'Profile',
-						link: '#',
-						class: 'disabled'
-					},
-					{
-						caption:'Settings',
-						link: '#',
-						class: 'disabled'
-					},
-					{
-						caption:'Sign Out',
-						link: '#',
-						class: ''
-					}
-				],
-				reference: 'Manage your profile'
-			}
-		],
-		current_view: 'Relationships',
-		panel_menus: [
-			{
-				panel: 'Relationships',
-				reference: 'List all your relationships',
-				icon: 'fa fa-users fa-lg',
-				class: 'btn btn-lg btn-primary btn-block',
-				length: 'col-md-4 col-sm-4 col-xs-4'
-			},
-			{
-				panel: 'Relationships',
-				reference: 'Add a relationship',
-				icon: 'fa fa-user-plus fa-lg',
-				class: 'btn btn-lg btn-info btn-block',
-				length: 'col-md-4 col-sm-4 col-xs-4'
-			},
-			{
-				panel: 'Relationships',
-				reference: 'Edit a relationship',
-				icon: 'fa fa-pencil-square fa-lg',
-				class: 'btn btn-lg btn-warning btn-block',
-				length: 'col-md-4 col-sm-4 col-xs-4'
-			}
-		]
-	},
-	computed: {
-		receivedPanels: function() {
-			for (i = 0; i < this.panel_menus.length; i++) {
-				var items = [];
-				items.push(this.panel_menus[i].panel)
-				return items
-			}
-		}
+		current_view: 'Relationships'
 	}
 });
