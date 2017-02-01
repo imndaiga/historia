f6253ec9a9acc2fdbf802306a86809f9f14bb173
Wasch <w.ndaiga@gmail.com>
2017-02-01 19:11:06 +0300

    all data encapsulated in app instance

--- static/js/script.js
+++ static/js/script.js
@@ -41,69 +41,23 @@ Vue.component('panel-navbar', {
 Vue.component('app-navbar', {
 	template: '#app-navbar',
 	props: {
-		menu_state: {
+		nav_state: {
 			type: Object,
 			required: true
 		},
 		open_submenu: {
 			type: String,
 			required: true
-		}
-	},
-	data: function() {
-		return {
-			title: 'MIMINANI',
-			menus: [
-				{
-					caption: 'Info',
-					icon: 'fa fa-info-circle fa-lg',
-					link: '#',
-					class: '',
-					dropdown: [],
-					reference: 'Read more about the miminani project'
-				},
-				{
-					caption: 'Github',
-					icon: 'fa fa-github fa-lg',
-					link: 'http://github.com/squarenomad/miminani',
-					class: '',
-					dropdown: [],
-					reference: 'View our open source code'
-				},
-				{
-					caption: 'Messages',
-					icon: 'fa fa-inbox fa-lg',
-					link: '#',
-					class: '',
-					dropdown: [],
-					reference: 'Notifications and alerts'
-				},
-				{
-					caption: 'User',
-					icon: 'fa fa-user-circle fa-lg',
-					link: '#',
-					class: 'dropdown active',
-					dropdown: [
-						{
-							caption:'Profile',
-							link: '#',
-							class: 'disabled'
-						},
-						{
-							caption:'Settings',
-							link: '#',
-							class: 'disabled'
-						},
-						{
-							caption:'Sign Out',
-							link: '#',
-							class: ''
-						}
-					],
-					reference: 'Manage your profile'
-				}
-		],
-		}
+		},
+		title: {
+			type: String,
+			required: true
+		},
+		nav_menus: {
+			type: Array,
+			required: true
+		},
+
 	},
 	methods: {
 		maindropdown: function() {
@@ -215,6 +169,57 @@ var vm = new Vue({
 				name: 'Share',
 				menus: [],
 				views: []
+			}
+		],
+		title: 'MIMINANI',
+		nav_menus: [
+			{
+				caption: 'Info',
+				icon: 'fa fa-info-circle fa-lg',
+				link: '#',
+				class: '',
+				dropdown: [],
+				reference: 'Read more about the miminani project'
+			},
+			{
+				caption: 'Github',
+				icon: 'fa fa-github fa-lg',
+				link: 'http://github.com/squarenomad/miminani',
+				class: '',
+				dropdown: [],
+				reference: 'View our open source code'
+			},
+			{
+				caption: 'Messages',
+				icon: 'fa fa-inbox fa-lg',
+				link: '#',
+				class: '',
+				dropdown: [],
+				reference: 'Notifications and alerts'
+			},
+			{
+				caption: 'User',
+				icon: 'fa fa-user-circle fa-lg',
+				link: '#',
+				class: 'dropdown active',
+				dropdown: [
+					{
+						caption:'Profile',
+						link: '#',
+						class: 'disabled'
+					},
+					{
+						caption:'Settings',
+						link: '#',
+						class: 'disabled'
+					},
+					{
+						caption:'Sign Out',
+						link: '#',
+						class: ''
+					}
+				],
+				reference: 'Manage your profile'
 			}
 		]
 	},
