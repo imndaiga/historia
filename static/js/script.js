1fa183009418f8c069866fb6f16c86415e9ee175
Wasch <w.ndaiga@gmail.com>
2017-02-01 12:21:02 +0300

    All components rendered in app root

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,18 @@
 Vue.component('panel-navbar', {
 	template: '#panel-navbar',
 	props: {
-		header: {
+		current_panel: {
 			type: String,
 			required: true
 		},
-		menus: {
+		button_menus: {
 			type: Array,
 			required: true
 		},
+		dropdown_menu: {
+			type: Array,
+			required: true
+		}
 	},
 	data: function() {
 		return {
@@ -20,7 +24,7 @@ Vue.component('panel-navbar', {
 			this.open_panel_menu = !this.open_panel_menu
 		}
 	}
-});
+})
 
 Vue.component('app-navbar', {
 	template: '#app-navbar',
@@ -97,7 +101,7 @@ Vue.component('app-navbar', {
 			this.$emit('subdropdown', menu)
 		}
 	}
-});
+})
 
 Vue.component('app-sidebar', {
 	template: "#app-sidebar",
@@ -105,17 +109,15 @@ Vue.component('app-sidebar', {
 		start_panel: {
 			type: String,
 			required: true
+		},
+		panels: {
+			type: Array,
+			required: true
 		}
 	},
 	data: function() {
 		return {
-			panels: [
-				'Overview',
-				'Relationships',
-				'Visualisation',
-				'Share',
-			],
-			active_panel: this.start_panel[0].toUpperCase() + this.start_panel.replace('-panel','').slice(1)
+			active_panel: this.start_panel
 		}
 	},
 	methods: {
@@ -131,81 +133,14 @@ Vue.component('app-sidebar', {
 			}
 		}
 	}
-});
-
-Vue.component('relationships-panel', {
-	template: '#relationships-panel',
-	data: function() {
-		return {
-			menus: [
-				{
-					name: 'List Relationships',
-					reference: 'List all your relationships',
-					icon: 'fa fa-users fa-lg',
-					class: 'btn btn-lg btn-primary btn-block',
-					length: 'col-md-4 col-sm-4 col-xs-4'
-				},
-				{
-					name: 'Add Relationships',
-					reference: 'Add a relationship',
-					icon: 'fa fa-user-plus fa-lg',
-					class: 'btn btn-lg btn-info btn-block',
-					length: 'col-md-4 col-sm-4 col-xs-4'
-				},
-				{
-					name: 'Edit Relationships',
-					reference: 'Edit a relationship',
-					icon: 'fa fa-pencil-square fa-lg',
-					class: 'btn btn-lg btn-warning btn-block',
-					length: 'col-md-4 col-sm-4 col-xs-4'
-				}
-			],
-			header: 'Relationships',
-			forms: [
-				{
-					parent_menu: 'Add Relationships',
-					inputs: [
-						{ placeholder: 'JohnOlooDoe@gmail.com', validate: "email" , name: 'Email'},
-						{ placeholder: 'John', validate: "alpha" , name: 'First Name'},
-						{ placeholder: 'Oloo', validate: "alpha" , name: 'Ethnic Name'},
-						{ placeholder: 'Doe', validate: "alpha" , name: 'Last Name'}
-					],
-					submit: 'Add'
-				}
-			]
-		}
-	}
 })
 
-Vue.component('overview-panel', {
-	template: '#overview-panel',
-	data: function() {
-		return {
-			menus: [],
-			header: 'Overview',
-			forms: []
-		}
-	}
-})
-
-Vue.component('visualisation-panel', {
-	template: '#visualisation-panel',
-	data: function() {
-		return {
-			menus: [],
-			header: 'Visualisation',
-			forms: []
-		}
-	}
-})
-
-Vue.component('share-panel', {
-	template: '#share-panel',
-	data: function() {
-		return {
-			menus: [],
-			header: 'Share',
-			forms: []
+Vue.component('form-pane', {
+	template: '#form-pane',
+	props: {
+		forms : {
+			type: Array,
+			required: true
 		}
 	}
 })
@@ -215,14 +150,65 @@ Vue.use(VeeValidate);
 var vm = new Vue({
 	el: '#app',
 	data: {
-		current_view: 'relationships-panel',
+		current_view: 'Relationships',
 		open_main_dropdown: false,
-		open_sub_dropdown_name: ''
+		open_sub_dropdown_name: '',
+		panels: [
+			{
+				name: 'Relationships',
+				menus: [
+					{
+						name: 'List Relationships',
+						reference: 'List all your relationships',
+						icon: 'fa fa-users fa-lg',
+						class: 'btn btn-lg btn-primary btn-block',
+						length: 'col-md-4 col-sm-4 col-xs-4'
+					},
+					{
+						name: 'Add Relationships',
+						reference: 'Add a relationship',
+						icon: 'fa fa-user-plus fa-lg',
+						class: 'btn btn-lg btn-info btn-block',
+						length: 'col-md-4 col-sm-4 col-xs-4'
+					},
+					{
+						name: 'Edit Relationships',
+						reference: 'Edit a relationship',
+						icon: 'fa fa-pencil-square fa-lg',
+						class: 'btn btn-lg btn-warning btn-block',
+						length: 'col-md-4 col-sm-4 col-xs-4'
+					}
+				],
+				forms: [
+					{
+						parent_menu: 'Add Relationships',
+						inputs: [
+							{ placeholder: 'JohnOlooDoe@gmail.com', validate: "email" , name: 'Email'},
+							{ placeholder: 'John', validate: "alpha" , name: 'First Name'},
+							{ placeholder: 'Oloo', validate: "alpha" , name: 'Ethnic Name'},
+							{ placeholder: 'Doe', validate: "alpha" , name: 'Last Name'}
+						],
+						submit: 'Add'
+					}
+				]
+			},{
+				name: 'Overview',
+				menus: [],
+				forms: []
+			},{
+				name: 'Visualisation',
+				menus: [],
+				forms: []
+			},{
+				name: 'Share',
+				menus: [],
+				forms: []
+			}
+		]
 	},
 	methods: {
 		updateView: function(panel_name) {
-			panel = panel_name.toLowerCase() + '-panel'
-			this.current_view = panel
+			this.current_view = panel_name
 			this.open_main_dropdown = false
 			this.open_sub_dropdown_name = ''
 		},
@@ -237,5 +223,28 @@ var vm = new Vue({
 				this.open_sub_dropdown_name = menu_name
 			}
 		}
+	},
+	computed: {
+		allPanelNames: function() {
+			panel_names_array = []
+			for (i = 0; i < this.panels.length; i++) {
+				panel_names_array.push(this.panels[i].name)
+			}
+			return panel_names_array
+		},
+		currentPanelMenus: function() {
+			for (i = 0; i < this.panels.length; i++) {
+				if (this.panels[i].name == this.current_view) {
+					return this.panels[i].menus
+				}
+			}
+		},
+		currentPanelForms: function() {
+			for (i=0; i < this.panels.length; i++) {
+				if (this.panels[i].name == this.current_view) {
+					return this.panels[i].forms
+				}
+			}
+		}
 	}
-});
+})
