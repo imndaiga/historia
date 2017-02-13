b3a3dfa326b3919af34afcf8541dbe506ecb1311
Wasch <w.ndaiga@gmail.com>
2017-02-13 05:54:41 +0300

    working select field

--- app/user/forms.py
+++ app/user/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import Required, Email, Length
 
 
 class AddPersonForm(FlaskForm):
-    sex_choices = [(1, 'Select Any'), (2, 'Male'), (3, 'Female')]
+    sex_choices = [(1, "Male"), (2, "Female")]
     baptism_name = StringField('Baptism Name', validators=[
         Required(),
         Length(1, 64)])
@@ -14,7 +14,8 @@ class AddPersonForm(FlaskForm):
     surname = StringField('Surname/Family Name', validators=[
         Required(),
         Length(1, 64)])
-    sex = SelectField(choices=sex_choices, default=['1'])
+    sex = SelectField('Sex', choices=sex_choices, default=1, validators=[
+        Required()])
     date_of_birth = DateField('Date of Birth')
     email = StringField('Email', validators=[Email(), Length(1, 64)])
     submit = SubmitField('Add')
