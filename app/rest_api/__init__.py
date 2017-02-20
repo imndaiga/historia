ffba74ccbd8cd5d1be13fff7ea0500639f981e68
Wasch <w.ndaiga@gmail.com>
2017-02-20 13:20:32 +0300

    global CORS for API blueprint

--- app/rest_api/__init__.py
+++ app/rest_api/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
+from flask_cors import CORS
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
+CORS(api_bp)
 
 from . import endpoints, forms
--- app/rest_api/endpoints.py
+++ app/rest_api/endpoints.py
@@ -1,11 +1,9 @@
 from . import api
 from flask_restful import Resource
-from flask_cors import cross_origin
 from .decorators import requires_auth
 
 
 class UserAPI(Resource):
-    decorators = [cross_origin(headers=['Content-Type', 'Authorization'])]
 
     def get(self, id):
         return "Hello, World!"
@@ -18,8 +16,7 @@ class UserAPI(Resource):
 
 
 class pingAPI(Resource):
-    decorators = [cross_origin(headers=['Content-Type', 'Authorization']),
-                  requires_auth]
+    decorators = [requires_auth]
 
     def get(self):
         return 'Here is a ping'
