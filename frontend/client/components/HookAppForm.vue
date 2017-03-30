8a0782ce2aea52b6e830e724ad3c9d7cdcc571ca
squarenomad <w.ndaiga@gmail.com>
2017-03-31 00:43:25 +0300

    implemented hook-app-form component (hydration errors need fixing)

---
+++ frontend/client/components/HookAppForm.vue
@@ -0,0 +1,183 @@
+<template id="app-form">
+  <form v-on:submit.prevent="submitForm" :class="{'form-inline': form_is_inline}">
+    <div v-for="field in form" v-if="validateField(field.field_name).exists" :class="['form-group', field.classes, {'has-error': validateField(field.field_name).has_error}]">
+      <div v-if="field.type == 'hidden-input'" style="display: none;" :key="field.key">
+        <label>{{ field.label }}</label>
+        <input type="text" :name="field.field_name" :value="field.placeholder" readonly>
+      </div>
+      <label v-else>{{ field.label }}</label>
+      <span class="form-group__message" v-if="validateField(field.field_name).is_required"> *</span>
+      <div v-if="['alpha-input', 'email-input'].indexOf(field.type) != -1" :class="{'input-group': field.value != undefined}">
+        <input type="text" class="form-control" :name="field.field_name" v-model.trim="form_object[field.field_name].value" :placeholder="field.placeholder" v-on:input="touchField(field.field_name)" :key="field.key" :readonly="field.value != undefined && !form_object[field.field_name].activated">
+        <div v-if="field.value != undefined" class="input-group-btn">
+          <button type="button" class="btn btn-warning" v-on:click="activateField(field.field_name)">
+            Change
+          </button>
+        </div>
+      </div>
+      <!-- <pre v-if="!!$v.form_object[field.field_name].value">{{$v.form_object[field.field_name].value}}</pre> -->
+      <span class="form-group__message" v-if="validateField(field.field_name).failed_required">{{ field.label }} is required</span>
+      <span class="form-group__message" v-else-if="validateField(field.field_name).failed_alpha">{{ field.label }} is not valid</span>
+      <span class="form-group__message" v-else-if="validateField(field.field_name).failed_email">Invalid email address</span>
+    </div>
+    <div v-for="field in form" v-if="field.type == 'submit-button'" class="form-group">
+      <button type="submit" :class="field.button_class">{{ field.button_message }}</button>
+    </div>
+  </form>
+</template>
+
+<script>
+  var vuelidate
+  if (process.browser) {
+    vuelidate = require('vuelidate')
+  }
+  export default {
+    mixins: [vuelidate ? vuelidate.validationMixin : ''],
+    props: {
+      form: {
+        type: Array,
+        required: true
+      },
+      submit_resource: {
+        type: String,
+        required: true
+      },
+      search_resource: {
+        type: String,
+        required: true
+      },
+      form_is_inline: {
+        type: Boolean,
+        required: true
+      }
+    },
+    data: function () {
+      return {
+        form_object: this.createFormObject()
+      }
+    },
+    validations: function () {
+      var validationsDict = {}
+      validationsDict['form_object'] = {}
+      for (var index in this.form) {
+        var key = this.form[index].field_name
+        if (key !== undefined) {
+          validationsDict.form_object[key] = {}
+          validationsDict.form_object[key].value = {}
+          if (Object.keys(this.form[index]).indexOf('validators') !== -1) {
+            var validators = this.form[index].validators
+            validationsDict.form_object[key].value = validators
+          }
+        }
+      }
+      return validationsDict
+    },
+    methods: {
+      submitForm: function () {
+        if (!this.$v.$invalid) {
+          var formData = {}
+          for (var field in this.form_object) {
+            formData[field] = this.form_object[field].value
+          }
+          // Send form data to submit resource
+        } else {
+          this.$v.$touch()
+        }
+      },
+      asyncFind: function (fieldName, value) {
+        this.form_object[fieldName].loading = true
+        // Perform full text search here
+      },
+      createFormObject: function () {
+        var formObject = {}
+        for (var index in this.form) {
+          var key = this.form[index].field_name
+          if (this.form[index].type !== 'submit-button') {
+            formObject[key] = {}
+            if (this.form[index].type === 'search-input') {
+              formObject[key]['loading'] = false
+              formObject[key]['options'] = []
+            }
+            if (Object.keys(this.form[index]).indexOf('value') !== -1) {
+              formObject[key]['value'] = this.form[index].value
+            } else {
+              formObject[key]['value'] = ''
+            }
+            formObject[key]['activated'] = false
+          }
+        }
+        return formObject
+      },
+      activateField: function (fieldName) {
+        this.form_object[fieldName].activated = true
+        this.$nuxt.$emit('form-field-activated')
+      },
+      validateField: function (fieldName) {
+        // Replace with better implementation, creates hydration errors
+        var validation = {}
+        if (this.$v && fieldName) {
+          validation.exists = this.$v.form_object[fieldName] !== null
+          validation.has_error = this.$v.form_object[fieldName].$error
+          validation.is_dirty = this.$v.form_object[fieldName].value.$dirty
+          validation.is_required = Object.keys(this.$v.form_object[fieldName].value).indexOf('required') !== -1
+          validation.is_alpha = Object.keys(this.$v.form_object[fieldName].value).indexOf('alpha') !== -1
+          validation.is_email = Object.keys(this.$v.form_object[fieldName].value).indexOf('email') !== -1
+          validation.failed_required = !this.$v.form_object[fieldName].value.required && validation.is_required && validation.is_dirty
+          validation.failed_alpha = !this.$v.form_object[fieldName].value.alpha && validation.is_alpha && validation.is_dirty
+          validation.failed_email = !this.$v.form_object[fieldName].value.email && validation.is_email && validation.is_dirty
+        } else {
+          validation.exists = false
+          validation.has_error = false
+          validation.is_dirty = false
+          validation.is_required = false
+          validation.is_alpha = false
+          validation.is_email = false
+          validation.failed_required = false
+          validation.failed_alpha = false
+          validation.failed_email = false
+        }
+        return validation
+      },
+      touchField: function (fieldName) {
+        // Replace with better implementation, creates hydration errors
+        if (this.$v && fieldName) {
+          this.$v.form_object[fieldName].value.$touch()
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  label {
+    font-size: 15px;
+    font-weight: 600
+  }
+  .form-control {
+    margin-bottom: 2px;
+    border-radius: 5px;
+    border: 1px solid rgba(232, 232, 232, 1);
+    min-height: 40px;
+    box-shadow: none
+  }
+  .form-control[disabled], .form-control[readonly], fieldset[disabled] .form-control {
+    opacity: 0.6;
+    background-color: #fff;
+    pointer-events: none
+  }
+
+  .input-group-btn button {
+    min-height: 40px
+  }
+
+  .disabled-button {
+    display: none
+  }
+
+  .form-group__message {
+    color: red;
+    font-weight: 400;
+    word-spacing: 1px;
+    font-size: 90%
+  }
+</style>
--- frontend/client/pages/user/home.vue
+++ frontend/client/pages/user/home.vue
@@ -3,7 +3,9 @@
     <page-header></page-header>
     <div class="panel-group">
       <div v-for="panel in panels" class="col-lg-6 col-md-6 col-sm-6">
-        <base-panel :panel="panel" :key="panel.key"></base-panel>
+        <base-panel :panel="panel" :key="panel.key">
+          <hook-app-form :form="panel.form" :submit_resource="panel.submit_resource" :search_resource="panel.search_resource" :form_is_inline="false"></hook-app-form>
+        </base-panel>
       </div>
     </div>
   </div>
@@ -12,17 +14,133 @@
 <script>
   import PageHeader from '~components/PageHeader.vue'
   import BasePanel from '~components/BasePanel.vue'
+  import HookAppForm from '~components/HookAppForm.vue'
+  import { required, email, alpha } from 'vuelidate/lib/validators'
   export default {
     layout: 'dashboard',
     components: {
       PageHeader: PageHeader,
-      BasePanel: BasePanel
+      BasePanel: BasePanel,
+      HookAppForm: HookAppForm
     },
     data: function () {
       return {
         panels: [
-          {name: 'add_relative_panel', open: false, label: 'Add Relative', icon: 'fa fa-user-plus fa-fw', key: 1},
-          {name: 'add_relationship_panel', open: false, label: 'Add Relationship', icon: 'fa fa-link fa-fw', key: 2}
+          {
+            name: 'add_relative_panel',
+            open: false,
+            label: 'Add Relative',
+            icon: 'fa fa-user-plus fa-fw',
+            submit_resource: '',
+            search_resource: '',
+            form: [
+              {
+                type: 'alpha-input',
+                placeholder: 'Enter First Name',
+                label: 'First Name',
+                validators: { required, alpha },
+                field_name: 'first_name',
+                classes: '',
+                key: 1
+              },
+              {
+                type: 'alpha-input',
+                placeholder: 'Enter Ethnic Name',
+                label: 'Ethnic Name',
+                validators: { required, alpha },
+                field_name: 'ethnic_name',
+                classes: '',
+                key: 2
+              },
+              {
+                type: 'alpha-input',
+                placeholder: 'Enter Last Name',
+                label: 'Last Name',
+                validators: { required, alpha },
+                field_name: 'last_name',
+                classes: '',
+                key: 3
+              },
+              {
+                type: 'email-input',
+                placeholder: 'Enter Email Address',
+                label: 'Email',
+                validators: { email },
+                field_name: 'email',
+                classes: '',
+                key: 4
+              },
+              {
+                type: 'pikaday-input',
+                placeholder: 'Select Birth Date',
+                label: 'Date of Birth',
+                field_name: 'birth_date',
+                classes: '',
+                key: 8
+              },
+              {
+                type: 'submit-button',
+                button_message: 'Save Relative',
+                button_name: 'add_personal-details',
+                button_class: 'btn btn-lg btn-success btn-block btn-margin',
+                key: 9
+              }
+            ],
+            key: 1
+          },
+          {
+            name: 'add_relationship_panel',
+            open: false,
+            label: 'Add Relationship',
+            icon: 'fa fa-link fa-fw',
+            submit_resource: '',
+            search_resource: '',
+            form: [
+              {
+                type: 'search-input',
+                placeholder: 'Search for Relative',
+                label: 'From',
+                validators: { required },
+                field_name: 'from_person',
+                classes: '',
+                SelectLabel: '',
+                DeselectLabel: '',
+                key: 5
+              },
+              {
+                type: 'search-input',
+                placeholder: 'Search for Relative',
+                label: 'To',
+                validators: { required },
+                field_name: 'to_person',
+                classes: '',
+                SelectLabel: '',
+                DeselectLabel: '',
+                key: 6
+              },
+              {
+                type: 'multiselect-input',
+                placeholder: 'Choose a Relation',
+                label: 'Relation',
+                validators: { required },
+                field_name: 'relation_name',
+                classes: '',
+                multiselect_options: ['Parent', 'Partner', 'Sibling', 'Step-Parent', 'Step-Sibling', 'Child'],
+                SelectLabel: '',
+                DeselectLabel: '',
+                key: 7
+              },
+              {
+                type: 'submit-button',
+                button_message: 'Save Relationship',
+                button_name: 'add_connect-relations',
+                bs_panel: 'add_relationship_panel',
+                button_class: 'btn btn-lg btn-success btn-block btn-margin',
+                key: 10
+              }
+            ],
+            key: 2
+          }
         ]
       }
     }
