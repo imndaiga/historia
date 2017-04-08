23b91900814523cd1dddce45be1d5a9f69159309
squarenomad <w.ndaiga@gmail.com>
2017-04-08 14:55:32 +0300

    changed alert-message dismissable default

--- frontend/client/store/index.js
+++ frontend/client/store/index.js
@@ -1,51 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
 
 Vue.use(Vuex)
 
-var server = axios.create()
-
 const store = new Vuex.Store({
   state: {
-    authUser: null,
     alert: {
       message: '',
       type: '',
       duration: 0,
-      dismissable: true
+      dismissable: false
     }
   },
   mutations: {
-    SET_USER: function (state, user) {
-      state.authUser = user
-    },
     SET_ALERT: function (state, alertObject) {
       state.alert = alertObject
     }
   },
   actions: {
-    login: function ({ commit }, { email, password }) {
-      return server.post('/auth/login', {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        email: email,
-        password: password
-      })
-      .then(function (response) {
-        commit('SET_USER', response.data.user)
-        localStorage.setItem('id_token', response.data.token)
-        return response
-      })
-      .catch(function (error) {
-        return error
-      })
-    },
-    logout: function ({ commit }) {
-      commit('SET_USER', null)
-      localStorage.removeItem('id_token')
-    },
     alert: function ({ commit }, { message, type, duration, dismissable }) {
       return commit('SET_ALERT', {
         message: message,
@@ -59,13 +31,8 @@ const store = new Vuex.Store({
         message: '',
         type: '',
         duration: 0,
-        dismissable: true
+        dismissable: false
       })
-    }
-  },
-  getters: {
-    isAuthenticated: function (state) {
-      return !!state.authUser && !!localStorage.getItem('id_token')
     }
   }
 })
