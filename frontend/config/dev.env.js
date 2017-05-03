24b2e828abd435d75a9040e8474560f25eea426e
squarenomad <w.ndaiga@gmail.com>
2017-05-03 16:11:13 +0300

    pull AUTH0 config from environment

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  AUTH0_ID: '""',
-  AUTH0_DOMAIN: '""',
+  AUTH0_ID: process.env.AUTH0_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
 })
