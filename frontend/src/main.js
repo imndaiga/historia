b25318a1b9ff64cdc5e764ff57c1c6667ca7089f
squarenomad <w.ndaiga@gmail.com>
2017-05-05 09:53:58 +0300

    attached vue-awesome library to VUe instance

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Icon from 'vue-awesome/components/Icon'
+
 require('bootstrap-css-only')
 
 Vue.config.productionTip = false
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
