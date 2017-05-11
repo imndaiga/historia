252fc58cc2f362201b29972e30c185475c43fb8c
squarenomad <w.ndaiga@gmail.com>
2017-05-11 10:31:58 +0300

    dev.env.js is a child of prod.env.js

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -2,7 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  AUTH0_ID: process.env.AUTH0_ID,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  NODE_ENV: '"development"'
 })
--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,3 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  AUTH0_ID: process.env.AUTH0_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
 }
