135d487d0dd5d525b397ea7f66f37b8f9458a9dc
squarenomad <w.ndaiga@gmail.com>
2017-05-12 11:09:58 +0300

    setup axios response interceptor for unathorized calls

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,30 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Icon from 'vue-awesome/components/Icon'
+import axios from 'axios'
+import auth from './utils/auth'
+
+var HTTP = axios.create({
+  baseURL: process.env.backendUrl
+})
+
+HTTP.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  // Do something with backend response error
+  if (error.response.status === 401) {
+    console.log('backend says: unauthorized, logging out ...')
+    auth.logout()
+    router.replace('/')
+  }
+  return Promise.reject(error)
+})
 
 require('bootstrap-css-only')
 
 Vue.config.productionTip = false
 Vue.component('icon', Icon)
+Vue.prototype.$http = HTTP
 
 /* eslint-disable no-new */
 new Vue({
