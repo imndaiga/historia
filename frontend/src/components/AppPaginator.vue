4dcdb2577bc7f4ee031e597c1eac8517477ebf94
squarenomad <w.ndaiga@gmail.com>
2017-05-16 11:39:37 +0300

    app-paginator and app-table components implemented

---
+++ frontend/src/components/AppPaginator.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="app-paginator">
+    <button class="btn btn-default" v-on:click="fetchData(prev_page_url)" :disabled="!prev_page_url">
+      {{config.previous_button_text}}
+      <icon v-if="config.previous_button_text.length === 0" name="chevron-left"></icon>
+    </button>
+    <span>Page {{current_page}} of {{last_page}}</span>
+    <button class="btn btn-default" v-on:click="fetchData(next_page_url)" :disabled="!next_page_url">
+      {{config.next_button_text}}
+      <icon v-if="config.next_button_text.length === 0" name="chevron-right"></icon>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource_url: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Object,
+      required: false,
+      default () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      current_page: '',
+      last_page: '',
+      next_page_url: '',
+      prev_page_url: '',
+      config: {
+        remote_data: 'data',
+        remote_current_page: 'current_page',
+        remote_last_page: 'last_page',
+        remote_next_page_url: 'next_page_url',
+        remote_prev_page_url: 'prev_page_url',
+        previous_button_text: 'Previous',
+        next_button_text: 'Next'
+      }
+    }
+  },
+  methods: {
+    fetchData (pageUrl) {
+      this.$emit('request-start')
+      pageUrl = pageUrl || this.resource_url
+      var self = this
+      this.$http.get(pageUrl, { headers: this.config.headers })
+      .then(function (response) {
+        self.$emit('request-finish', response)
+        self.handleResponseData(response.data)
+      }).catch(function (response) {
+        self.$emit('request-error', response)
+        console.log('Fetching data failed.', response)
+      })
+    },
+    handleResponseData (response) {
+      this.makePagination(response)
+      let data = this.getNestedValue(response, this.config.remote_data)
+      this.$emit('update', data)
+    },
+    makePagination (data) {
+      this.current_page = this.getNestedValue(data, this.config.remote_current_page)
+      this.last_page = this.getNestedValue(data, this.config.remote_last_page)
+      this.next_page_url = (this.current_page === this.last_page) ? null : this.getNestedValue(data, this.config.remote_next_page_url)
+      this.prev_page_url = (this.current_page === 1) ? null : this.getNestedValue(data, this.config.remote_prev_page_url)
+    },
+    initConfig () {
+      this.config = this.merge_objects(this.config, this.options)
+    },
+    merge_objects (obj1, obj2) {
+      let obj3 = {}
+      for (let attrname in obj1) { obj3[attrname] = obj1[attrname] }
+      for (let attrname in obj2) { obj3[attrname] = obj2[attrname] }
+      return obj3
+    },
+    getNestedValue (obj, path) {
+      let originalPath = path
+      path = path.split('.')
+      let res = obj
+      for (let i = 0; i < path.length; i++) {
+        res = res[path[i]]
+      }
+      if (typeof res === 'undefined') {
+        console.log(`Response doesn't contain key ${originalPath}!`)
+      }
+      return res
+    }
+  },
+  watch: {
+    resource_url () {
+      this.fetchData()
+    }
+  },
+  created () {
+    this.initConfig()
+    this.fetchData()
+  }
+}
+</script>
+
+<style scoped>
+  .fa-icon {
+    width: auto;
+    height: 1em;
+    margin: 3px;
+  }
+  button {
+    font-size: 15px;
+    line-height: 1px;
+  }
+</style>
---
+++ frontend/src/components/AppTable.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="app-table">
+    <div v-if="table_Data.length > 0">
+      <div class="table-responsive">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th class="center-content" v-for="header in table_Headers">{{header}}</th>
+              <th class="center-content">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="record in table_Data">
+              <td class="center-content" v-for="(value, key) in record" v-if="key != 'id'">{{ value }}</td>
+              <td class="center-content">
+                <button type="button" v-on:click="openRecord(record.id, [record.first_name, record.ethnic_name, record.last_name], '/api/person')" class="btn btn-default btn-sm" title="Edit">
+                  <icon name="user"></icon>
+                </button>
+                <button type="button" v-on:click="openRecord(record.id,  [record.first_name, record.ethnic_name, record.last_name], 'api/person/family')" class="btn btn-default btn-sm" title="Edit">
+                  <icon name="link"></icon>
+                </button>
+                <button type="button" class="btn btn-danger btn-sm" v-on:click="deleteRecord(record.id)" title="Delete">
+                  <icon name="trash-o"></icon>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <p v-else class="page-message text-center">No Relationship Data Available</p>
+    <div class="row" v-show="table_Data.length > 0">
+      <app-paginator class="col-lg-3 col-lg-push-5 col-md-4 col-md-push-4 col-sm-5 col-sm-push-4 col-xs-6 col-xs-push-4" :resource_url="resource_url" v-on:update="updateTable" :options="options"></app-paginator>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AppPaginator from './AppPaginator.vue'
+  import bus from '../utils/bus'
+
+  export default {
+    components: {
+      AppPaginator: AppPaginator
+    },
+    props: {
+      resource_url: {
+        type: String,
+        required: true
+      }
+    },
+    data: function () {
+      return {
+        raw_table_data: [],
+        options: {
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('id_token')
+          },
+          next_button_text: '',
+          previous_button_text: ''
+        }
+      }
+    },
+    methods: {
+      updateTable: function (data) {
+        this.raw_table_data = data
+      },
+      openRecord: function (personId, nameList, resourceUrl) {
+        var fullName = nameList.filter(function (val) { return val }).join(' ')
+        this.$http.get(resourceUrl, {
+          params: {
+            id: personId
+          }
+        })
+        .then(function (response) {
+          var form = response.data.form
+          // var inline = response.data.inline
+          bus.$emit('modal-data-ready', fullName, 'edit', form, 'Save Changes', null)
+        }).catch(function (error) {
+          console.log(error)
+        })
+      },
+      deleteRecord: function (recordId) {
+        var self = this
+        this.$http.delete(this.resource_url, {
+          data: {
+            id: recordId
+          }
+        })
+        .then(function (response) {
+          for (var record in self.raw_table_data) {
+            for (var field in self.raw_table_data[record]) {
+              if (self.raw_table_data[record][field].field_name === 'id') {
+                if (self.raw_table_data[record][field].value === recordId) {
+                  self.raw_table_data.splice(record, 1)
+                }
+              }
+            }
+          }
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
+    },
+    computed: {
+      table_Headers: function () {
+        var headers = []
+        for (var record in this.raw_table_data) {
+          for (var field in this.raw_table_data[record]) {
+            var header = this.raw_table_data[record][field].label
+            if (headers.indexOf(header) === -1 && header !== 'ID') {
+              headers.push(this.raw_table_data[record][field].label)
+            }
+          }
+        }
+        return headers
+      },
+      table_Data: function () {
+        var records = []
+        var entry = {}
+        for (var record in this.raw_table_data) {
+          var fieldName = ''
+          var fieldValue = ''
+          for (var field in this.raw_table_data[record]) {
+            fieldName = this.raw_table_data[record][field].field_name
+            fieldValue = this.raw_table_data[record][field].value
+            entry[fieldName] = fieldValue
+          }
+          records.push(entry)
+          entry = {}
+        }
+        return records
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .fa-icon {
+    width: auto;
+    height: 1em;
+  }
+
+  button {
+    font-size: 15px;
+  }
+
+  .page-message {
+    margin-top: 20%;
+    font-size: 20px
+  }
+
+  .app-table {
+    margin: 4%;
+  }
+</style>
