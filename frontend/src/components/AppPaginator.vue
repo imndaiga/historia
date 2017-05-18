61728001b1874a3b715bcb2bd91608c486821b6e
squarenomad <w.ndaiga@gmail.com>
2017-05-18 16:44:53 +0300

    indent script content

--- frontend/src/components/AppPaginator.vue
+++ frontend/src/components/AppPaginator.vue
@@ -13,94 +13,94 @@
 </template>
 
 <script>
-export default {
-  props: {
-    resource_url: {
-      type: String,
-      required: true
-    },
-    options: {
-      type: Object,
-      required: false,
-      default () {
-        return {}
+  export default {
+    props: {
+      resource_url: {
+        type: String,
+        required: true
+      },
+      options: {
+        type: Object,
+        required: false,
+        default () {
+          return {}
+        }
       }
-    }
-  },
-  data () {
-    return {
-      current_page: '',
-      last_page: '',
-      next_page_url: '',
-      prev_page_url: '',
-      config: {
-        remote_data: 'data',
-        remote_current_page: 'current_page',
-        remote_last_page: 'last_page',
-        remote_next_page_url: 'next_page_url',
-        remote_prev_page_url: 'prev_page_url',
-        previous_button_text: 'Previous',
-        next_button_text: 'Next'
+    },
+    data () {
+      return {
+        current_page: '',
+        last_page: '',
+        next_page_url: '',
+        prev_page_url: '',
+        config: {
+          remote_data: 'data',
+          remote_current_page: 'current_page',
+          remote_last_page: 'last_page',
+          remote_next_page_url: 'next_page_url',
+          remote_prev_page_url: 'prev_page_url',
+          previous_button_text: 'Previous',
+          next_button_text: 'Next'
+        }
       }
-    }
-  },
-  methods: {
-    fetchData (pageUrl) {
-      this.$emit('request-start')
-      pageUrl = pageUrl || this.resource_url
-      var self = this
-      this.$http.get(pageUrl, { headers: this.config.headers })
-      .then(function (response) {
-        self.$emit('request-finish', response)
-        self.handleResponseData(response.data)
-      }).catch(function (response) {
-        self.$emit('request-error', response)
-        console.log('Fetching data failed.', response)
-      })
     },
-    handleResponseData (response) {
-      this.makePagination(response)
-      let data = this.getNestedValue(response, this.config.remote_data)
-      this.$emit('update', data)
-    },
-    makePagination (data) {
-      this.current_page = this.getNestedValue(data, this.config.remote_current_page)
-      this.last_page = this.getNestedValue(data, this.config.remote_last_page)
-      this.next_page_url = (this.current_page === this.last_page) ? null : this.getNestedValue(data, this.config.remote_next_page_url)
-      this.prev_page_url = (this.current_page === 1) ? null : this.getNestedValue(data, this.config.remote_prev_page_url)
-    },
-    initConfig () {
-      this.config = this.merge_objects(this.config, this.options)
-    },
-    merge_objects (obj1, obj2) {
-      let obj3 = {}
-      for (let attrname in obj1) { obj3[attrname] = obj1[attrname] }
-      for (let attrname in obj2) { obj3[attrname] = obj2[attrname] }
-      return obj3
-    },
-    getNestedValue (obj, path) {
-      let originalPath = path
-      path = path.split('.')
-      let res = obj
-      for (let i = 0; i < path.length; i++) {
-        res = res[path[i]]
+    methods: {
+      fetchData (pageUrl) {
+        this.$emit('request-start')
+        pageUrl = pageUrl || this.resource_url
+        var self = this
+        this.$http.get(pageUrl, { headers: this.config.headers })
+        .then(function (response) {
+          self.$emit('request-finish', response)
+          self.handleResponseData(response.data)
+        }).catch(function (response) {
+          self.$emit('request-error', response)
+          console.log('Fetching data failed.', response)
+        })
+      },
+      handleResponseData (response) {
+        this.makePagination(response)
+        let data = this.getNestedValue(response, this.config.remote_data)
+        this.$emit('update', data)
+      },
+      makePagination (data) {
+        this.current_page = this.getNestedValue(data, this.config.remote_current_page)
+        this.last_page = this.getNestedValue(data, this.config.remote_last_page)
+        this.next_page_url = (this.current_page === this.last_page) ? null : this.getNestedValue(data, this.config.remote_next_page_url)
+        this.prev_page_url = (this.current_page === 1) ? null : this.getNestedValue(data, this.config.remote_prev_page_url)
+      },
+      initConfig () {
+        this.config = this.merge_objects(this.config, this.options)
+      },
+      merge_objects (obj1, obj2) {
+        let obj3 = {}
+        for (let attrname in obj1) { obj3[attrname] = obj1[attrname] }
+        for (let attrname in obj2) { obj3[attrname] = obj2[attrname] }
+        return obj3
+      },
+      getNestedValue (obj, path) {
+        let originalPath = path
+        path = path.split('.')
+        let res = obj
+        for (let i = 0; i < path.length; i++) {
+          res = res[path[i]]
+        }
+        if (typeof res === 'undefined') {
+          console.log(`Response doesn't contain key ${originalPath}!`)
+        }
+        return res
       }
-      if (typeof res === 'undefined') {
-        console.log(`Response doesn't contain key ${originalPath}!`)
+    },
+    watch: {
+      resource_url () {
+        this.fetchData()
       }
-      return res
-    }
-  },
-  watch: {
-    resource_url () {
+    },
+    created () {
+      this.initConfig()
       this.fetchData()
     }
-  },
-  created () {
-    this.initConfig()
-    this.fetchData()
   }
-}
 </script>
 
 <style scoped>
