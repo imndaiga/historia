fad3107b80b79f05d69f9bb5ac1f8ad338f5574b
squarenomad <w.ndaiga@gmail.com>
2017-05-18 15:09:28 +0300

    app-reload component created

---
+++ frontend/src/components/AppReload.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="text-center">
+    <p class="page-message">{{ load_message }}</p>
+    <a v-on:click="reloadResource">
+      <icon name="refresh"></icon>
+      <span class="reload-text">Reload</span>
+    </a>  
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      message: {
+        required: true,
+        type: String
+      }
+    },
+    data: function () {
+      return {
+        load_message: this.message
+      }
+    },
+    methods: {
+      reloadResource: function () {
+        this.$emit('reload-resource')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .page-message {
+    margin-top: 25%;
+    font-size: 20px;
+  }
+  a {
+    text-decoration: none;
+    color: grey;
+    cursor: pointer;
+    font-size: 18px;
+  }
+  a:hover,
+  a:focus {
+    text-decoration: none;
+    color: black;
+  }
+  .fa-icon {
+    padding-top: 4px;
+    margin-right: 5px;
+    width: auto;
+    height: 1em;
+  }
+</style>
