1e9ba0b50a11c92d6605d9fb7ef7b529d3b04b17
squarenomad <w.ndaiga@gmail.com>
2017-05-25 13:57:25 +0300

    implemented app-card component

---
+++ frontend/src/components/AppCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+    <div class="panel panel-default">
+      <div v-if="displayHeader" class="panel-heading">
+        <icon v-if="headerIcon" :name="headerIcon"></icon>
+        <span v-if="header" class="header-text">{{ header }}</span>
+      </div>
+      <div class="panel-body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      header: {
+        type: String,
+        required: false
+      },
+      headerIcon: {
+        type: String,
+        required: false
+      }
+    },
+    computed: {
+      displayHeader: function () {
+        return this.header && this.headerIcon
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .panel,
+  .panel .panel-heading,
+  .panel .panel-body {
+    border-radius: 0px
+  }
+  .fa-icon {
+    vertical-align: -4px
+  }
+  .panel-heading {
+    font-size: 19px
+  }
+  .header-text {
+    padding-left: 4%;
+    font-weight: 600;
+    font-size: 15px
+  }
+</style>
