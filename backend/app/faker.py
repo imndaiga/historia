3f3f9d93b2e3b23a69fb0a54300b3f352df53fc7
squarenomad <w.ndaiga@gmail.com>
2017-06-22 16:22:35 +0300

    improved member injection, implement email parameter in family_member

--- backend/app/faker.py
+++ backend/app/faker.py
@@ -5,41 +5,27 @@ fake = Factory.create('en_GB')
 
 
 class FamilyProvider(BaseProvider):
-    def family(self, seed, size, injection=[]):
+    def family(self, seed, size, inject=None):
         fake.seed(seed)
-        if len(injection) == 0:
-            parents = [self.family_member(sex='Male'),
-                       self.family_member(sex='Female')]
-            children = []
-            for _ in range(0, size - 2):
-                a_child = self.family_member(
-                    sex=fake.random.choice(['Male', 'Female']))
-                children.append(a_child)
-        else:
-            if injection[0] == 'parents':
-                parents = [self.family_member(sex='Male'),
-                           self.family_member(sex='Female')]
-                children = [injection[1]]
-                for _ in range(0, size - 3):
-                    a_child = self.family_member(
-                        sex=fake.random.choice(['Male', 'Female']))
-                    children.append(a_child)
-            else:
-                if injection[1]['sex'] == 'Male':
-                    parents = [injection[1],
-                               self.family_member(sex='Female')]
-                else:
-                    parents = [injection[1],
-                               self.family_member(sex='Male')]
-                children = []
-                for _ in range(0, size - 2):
-                    a_child = self.family_member(
-                        sex=fake.random.choice(['Male', 'Female']))
-                    children.append(a_child)
-        a_family = {'parents': parents, 'children': children}
-        return a_family
+        parents = [
+            self.family_member(sex='Male'),
+            self.family_member(sex='Female')
+        ]
+        children = []
+        for _ in range(0, size - 2):
+            a_child = self.family_member(
+                sex=fake.random.choice(['Male', 'Female']))
+            children.append(a_child)
 
-    def family_member(self, sex='Male'):
+        if inject:
+            if inject['type'] == 'parent':
+                parents[0] = inject['member']
+            elif inject['type'] == 'child':
+                children[0] = inject['member']
+        return {'parents': parents, 'children': children}
+
+    def family_member(self, email=None, sex='Male'):
+        family_member = {}
         if sex == 'Male':
             family_member = {
                 'first_name': fake.first_name_male(),
@@ -47,7 +33,7 @@ class FamilyProvider(BaseProvider):
                 'last_name': fake.last_name_male(),
                 'sex': 'Male',
                 'birth_date': fake.past_date(),
-                'email': fake.safe_email()
+                'email': email or fake.safe_email()
             }
         elif sex == 'Female':
             family_member = {
@@ -56,7 +42,7 @@ class FamilyProvider(BaseProvider):
                 'last_name': fake.last_name_female(),
                 'sex': 'Female',
                 'birth_date': fake.past_date(),
-                'email': fake.safe_email()
+                'email': email or fake.safe_email()
             }
         return family_member
 
